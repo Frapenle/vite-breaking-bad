b6204922ef721f6c31f840006a953a4f579cd87c
francescotrudu <francescotrudu84@gmail.com>
2023-01-11 16:00:48 +0100

    Add Header component

---
+++ src/components/HeaderVue.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderVue',
+    data() {
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style lang="" scoped>
+    
+</style>
