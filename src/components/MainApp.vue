30969cb9cd5a050fc9ac9cd66e7d7c2d02410313
francescotrudu <francescotrudu84@gmail.com>
2023-01-11 16:49:19 +0100

    Add methods and created hooks

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,18 +1,38 @@
 <script >
+import axios from 'axios'
+
 export default {
   name: 'MainApp',
   data() {
     return {
 
     }
+  },
+  //aggiungo le funzioni
+  methods: {
+    getCards() {
+
+    }
+  },
+  //aggiungo hook created per chiamare la funzione ad ogni caricamento della pagina
+  created() {
+    this.getCards();
   }
 }
 </script>
 
 <template>
-  <main>Main</main>
+  <main>
+    <div class="card-container">
+      <div class="cards">
+        <card />
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
-
+main {
+  padding: 0 1rem;
+}
 </style>
