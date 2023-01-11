d5afa66c31c55a64efb7eb7848a54589831e1a94
francescotrudu <francescotrudu84@gmail.com>
2023-01-11 15:57:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+// importo il component
+import MainApp from './components/MainApp.vue'
+
+// esporto il component
+export default {
+  components: {
+    MainApp
+  }
+}
+</script>
+
+<template>
+  <!-- aggiungo il component al template -->
+  <MainApp />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/variables" as *;
+</style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,18 @@
+<script >
+export default {
+  name: 'MainApp',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <main>Main</main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
