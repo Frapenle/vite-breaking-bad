569745c8b9d38cfdb9770d19c2ec860c24afcf95
francescotrudu <francescotrudu84@gmail.com>
2023-01-11 16:48:29 +0100

    Add card template

---
+++ src/components/CardApp.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div class="card">
+        <img src="" alt="">
+        <p class="text-name"></p>
+        <p class="text-type"></p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+
+</style>
