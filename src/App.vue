248f3deb5fc4c696961275014326af95ec28dece
francescotrudu <francescotrudu84@gmail.com>
2023-01-13 10:08:41 +0100

    Import axios and store

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script>
 // importo il component
+import { store } from "./store.js";
 import HeaderVue from './components/HeaderVue.vue';
-import MainApp from './components/MainApp.vue'
+import MainApp from './components/MainApp.vue';
+import axios from 'axios';
+
 
 // esporto il component
 export default {
@@ -9,24 +12,54 @@ export default {
     HeaderVue,
     MainApp,
   },
+
   data() {
     return {
-      selection: '',
+      store,
+      apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+      archetypeDefault: 'alien',
     }
   },
+
+  //aggiungo le funzioni
   methods: {
-    getSelection(params) {
-      this.selection = params;
-    }
+    getCards(archetypeSelection) {
+      axios.get(this.apiUrl, {
+        params: {
+          num: 20,
+          offset: 0,
+          archetype: archetypeSelection
+        }
+      })
+        .then((response) => {
+          console.log(response.data.data);
+          this.store.cardsList = response.data.data;
+          this.store.cardsNumber = this.store.cardsList.length;
+          console.log(this.store.cardsNumber)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    // getSelection(params) {
+    //   this.selection = params;
+    //   console.log(params)
+    // }
   },
+  //aggiungo hook created per chiamare la funzione ad ogni caricamento della pagina
+  created() {
+    this.getCards(this.archetypeDefault);
+  },
+
+
 }
 
 </script>
 
 <template>
   <!-- aggiungo i components al template -->
-  <HeaderVue @selection="getSelection" />
-  <MainApp :selection="selection" />
+  <HeaderVue />
+  <MainApp @selection="getCards" />
 </template>
 
 <style lang="scss">
